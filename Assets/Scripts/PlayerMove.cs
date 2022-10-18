0ff2c66293b4e30c09a425ab1933056663fce4ba
BrandoniousGit <72399815+BrandoniousGit@users.noreply.github.com>
2022-10-18 12:15:19 +0100

    Better movement
    Updated movement to flow better

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,7 +6,7 @@ public class PlayerMove : MonoBehaviour
 {
     private Camera cam;
     private Rigidbody rb;
-    public float moveSpeed, maxSpeed, jumpForce;
+    public float moveSpeed, maxSpeed, jumpForce, slowdownMulti;
     private bool jumping;
 
     private void Start()
@@ -26,37 +26,39 @@ public class PlayerMove : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
             Vector3 moveDir = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized;
 
-            rb.AddForce(moveDir * moveSpeed);
+            rb.AddForce(moveDir * moveSpeed * Time.deltaTime);
 
             if (rb.velocity.magnitude > maxSpeed)
             {
                 rb.velocity = new Vector3(maxSpeed * moveDir.x, rb.velocity.y, maxSpeed * moveDir.z);
             }
+        }
 
-            Debug.DrawRay(cam.transform.position, Quaternion.Euler(0, Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y, 0) * Vector3.forward, Color.yellow, Time.deltaTime);
+        else
+        {
+            rb.velocity = new Vector3(rb.velocity.x / slowdownMulti, rb.velocity.y, rb.velocity.z / slowdownMulti);
         }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        RaycastHit hit;
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumping == false)
         {
             rb.AddForce(0, jumpForce, 0);
             jumping = true;
+            slowdownMulti = 1.04f;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Physics.SphereCast(transform.position, transform.localScale.x / 2, Vector3.down, out hit, (transform.localScale.y / 2) + 0.05f) && jumping == true)
         {
             jumping = false;
-        }
-
-        if (jumping != true)
-        {
-            rb.velocity = new Vector3(rb.velocity.x / 1.45f, rb.velocity.y, rb.velocity.z / 1.45f);
+            slowdownMulti = 1.18f;
         }
     }
 
-    void FixedUpdate()
+    void LateUpdate()
     {
         UserInput();
     }
